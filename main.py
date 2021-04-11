7055b0216805b457258a040ffe8bc90b296d6876
juhyong <jhjh20652002@gmail.com>
2021-04-11 21:06:16 +0900

    Updates on Local Master Branch

--- main.py
+++ main.py
@@ -20,3 +20,6 @@ print("Hello World 5")
 
 #updates on main branch of GitHub
 print("Hello World Update on GitHub branch")
+
+#Updates in local master branch
+print("Hello World Updates on Local Master Branch")
